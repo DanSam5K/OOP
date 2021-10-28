502f3567e6ee932a5daeda72478dcb986c99e175
Daniel Samuel <dansam5000@gmail.com>
2021-10-28 17:48:47 +0100

    feat: Encapsulation by adding getter and setter

--- animal.rb
+++ animal.rb
@@ -1,16 +1,38 @@
 class Animal
-	def initialize (name = "Unknown", number_of_legs)
+	def initialize (type, number_of_legs, name = "Unknown")
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
+		@type = type
   end
 
 	def speak
     "Bla bla bla"
+  end
+
+	def id
+    @id
+  end
+
+  def type
+    @type
+  end
+
+  def number_of_legs
+    @number_of_legs
+  end
+
+  def name
+    @name
+  end
+
+  def name=(value)
+    @name = value
   end
 end
 
 animal_1 = Animal.new("Rex", 4)
 animal_2 = Animal.new("Bob", 8)
 
-puts animal_1
+puts animal_1.name=('Lion')
+puts animal_1.name
